0a0b9c858dc6a591c6bca89e1c5b2d1bd838525d
melek seba <melekseba34@gmail.com>
2023-07-10 23:03:10 -0400

    added project

---
+++ MelekDB~1.sql
@@ -0,0 +1,19 @@
+-- SELECT <columnName> FROM <tableName>;
+
+SELECT * FROM employees;
+
+-- SUGGESTED FORMAT --
+SELECT * FROM JOBS;
+
+select * from emplyoees;
+
+SeleCt *FrOm eMploYEES;
+
+SELECT * FROM region;
+
+SELECT manager_id FROM employees;
+
+SELECT first_name,hire_date FROM employees;
+
+
+
